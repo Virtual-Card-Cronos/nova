0e9bbd44815648b7dc61189200d5f1a7d5c60b65
Nanasark <danielnyamekyesarkodie@gmail.com>
2026-01-24 14:48:38 +0000

    remove webpack

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -12,6 +12,18 @@ const nextConfig = {
       config.resolve.alias = {
         ...config.resolve.alias,
         '@crypto.com/facilitator-client': false,
+        // Also ignore problematic ox package exports
+        'ox/erc8010': false,
+        'ox/tempo': false,
+        'ox/erc6492': false,
+      }
+    } else {
+      // Server-side: also ignore problematic ox package exports
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'ox/erc8010': false,
+        'ox/tempo': false,
+        'ox/erc6492': false,
       }
     }
 
@@ -20,20 +32,13 @@ const nextConfig = {
     config.plugins = config.plugins || []
     
     // Replace x402 module imports from thirdweb
+    const stubPath = path.resolve(__dirname, 'src/lib/x402-stub.js')
     config.plugins.push(
       new webpack.NormalModuleReplacementPlugin(
         /thirdweb\/dist\/esm\/.*\/x402/,
-        path.resolve(__dirname, 'src/lib/x402-stub.js')
+        stubPath
       )
     )
-    
-    // Also ignore problematic ox package exports
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'ox/erc8010': false,
-      'ox/tempo': false,
-      'ox/erc6492': false,
-    }
 
     return config
   },
